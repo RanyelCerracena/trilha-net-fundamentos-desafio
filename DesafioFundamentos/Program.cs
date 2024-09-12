f10dc2759b44314a628b5bfa8d5c7aa6413cae42
Ranyel Cerracena de Sousa <rcerracena@gmail.com>
2024-09-12 20:30:19 -0300

    Conclui a proposta do projeto, fazendo com que o sistema funcione corretamente. Não estava proposto, entretanto, tive a ousadia de criar um loop para o tratamento de uma possivel exceção no Program.cs, quando solicitado o valor, caso o usuário digitasse um valor invalido, o programa apenas fechava e apresentava erro, agora, o mesmo exibe uma mensagem informando o valor inválido e solicitando novamente o valor, até que seja informado um valor válido.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -6,12 +6,38 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!");
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Digite o preço inicial:");
+        precoInicial = Convert.ToDecimal(Console.ReadLine());
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("O valor informado é inválido. Por favor forneça um valor válido");
+    }
+
+}
+
+
+while (true)
+{
+    try
+    {
+        Console.WriteLine("Agora digite o preço por hora:");
+        precoPorHora = Convert.ToDecimal(Console.ReadLine());
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("O valor informado é inválido. Por favor forneça um valor válido");
+        throw;
+    }
+}
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
